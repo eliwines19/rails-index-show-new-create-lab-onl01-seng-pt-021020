e93f733d96a41a09c9be52202b4a9a851285d3f1
Elijah wines <elijahwines84@yahoo.com>
2020-06-16 15:26:46 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+resources :coupons only: [:index, :show, :new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,7 +15,6 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :coupons
 
   # Example resource route with options:
   #   resources :products do

b74a3e6e3a35c9ff3ff65061c35ff5040870724b
Elijah wines <elijahwines84@yahoo.com>
2020-06-16 14:54:40 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  resources :coupons, only: [:index, :show, :create, :new]
 end
